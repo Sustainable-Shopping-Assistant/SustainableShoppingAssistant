20d7770cfb786adc245f29895c28dfb3a94bc213
Chuck Song <chuckthedeveloper@gmail.com>
2023-04-08 22:24:49 -0400

    Added Signup button to Home page

--- EcoShop/src/pages/Home.tsx
+++ EcoShop/src/pages/Home.tsx
@@ -22,37 +22,41 @@ const Home: React.FC = () => {
           <Link to="/" className="navbar-brand">
             Home
           </Link>
-          {user ? (
-            <div className="dropdown">
-              <button
-                className="btn btn-secondary dropdown-toggle"
-                type="button"
-                id="dropdownMenuButton"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-                onClick={handleToggleDropdown}
-              >
-                {user.first_name} ▼ <span className="caret"></span>
+          <div className="auth-links">
+            {user ? (
+              <div className="dropdown">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  id="dropdownMenuButton"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  onClick={handleToggleDropdown}
+                >
+                  {user.first_name} ▼ <span className="caret"></span>
+                </button>
+                {showDropdown && (
+                  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <Link to="/shoppinglist" className="dropdown-item" onClick={handleHideDropdown}>
+                      Shopping List
+                    </Link>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <><Link to="/signup" className="auth-link">
+                  Sign up
+                </Link><Link to="/login" className="auth-link">
+                    Log in
+                  </Link></>
+            )}
+            {user && (
+              <button className="btn btn-primary ml-auto" onClick={logout}>
+                Log out
               </button>
-              {showDropdown && (
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <Link to="/shoppinglist" className="dropdown-item" onClick={handleHideDropdown}>
-                    Shopping List
-                  </Link>
-                </div>
-              )}
-            </div>
-          ) : (
-            <Link to="/login" className="nav-link">
-              Login
-            </Link>
-          )}
-          {user && (
-            <button className="btn btn-primary ml-auto" onClick={logout}>
-              Logout
-            </button>
-          )}
+            )}
+          </div>
         </nav>
       </header>
       <main>
